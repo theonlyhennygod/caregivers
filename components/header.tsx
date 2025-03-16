c0b4a164a400676400c4476ba66477bca39f3676
Argenis De La Rosa <theonlyhennygod@gmail.com>
2025-03-16 16:19:08 -0400

    updated all components

--- components/header.tsx
+++ components/header.tsx
@@ -5,7 +5,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Input } from "@/components/ui/input"
 import { Badge } from "@/components/ui/badge"
 import Link from "next/link"
-import { ThemeToggle } from "@/components/theme-toggle"
+import { ThemeToggle } from "@/components/theme-provider"
 
 interface HeaderProps {
   userName: string
--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -5,7 +5,7 @@ import { useTheme } from "next-themes"
 import { Button } from "@/components/ui/button"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 
-export function ThemeToggle() {
+export function ThemeProvider() {
   const { setTheme, theme } = useTheme()
 
   return (
